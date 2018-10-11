db2daa249487cc849816bc3d54383353f7366746
Murthy naika <murthy.naikak@capita.co.uk>
2018-10-11 16:55:21 +0530

    Scheduler UI CR:Sucheta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,96 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+  /**
+ * Constants for Days
+ */
+export enum Daylist {
+  'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'
+}
+/**
+* Constants for Months
+*/
+export enum MonthsList {
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public days = true;
+  public weeks: boolean;
+  public months: boolean;
+  public value: Date = new Date();
+  public dateFormat: any;
+
+  public evntData: any[] = [{
+    ShortDescription: 'Tree desc',
+    LocList: '01BN - 01 Berneslai Lifecycle-01',
+    color: 'bg-pink'
+  },
+  {
+    ShortDescription: 'Tree desc',
+    LocList: '01BN - 01 Berneslai Lifecycle-01',
+    color: 'bg-yellow'
+  },
+  {
+    ShortDescription: 'Tree desc',
+    LocList: '01BN - 01 Berneslai Lifecycle-01',
+    // color: 'bg-lime'
+  },
+  {
+    ShortDescription: 'Tree desc',
+    LocList: '01BN - 01 Berneslai Lifecycle-01',
+    color: 'bg-yellow'
+  },
+  {
+    ShortDescription: 'Tree desc',
+    LocList: '01BN - 01 Berneslai Lifecycle-01',
+    color: 'bg-lime'
+  },
+  {
+    ShortDescription: 'Tree desc',
+    LocList: '01BN - 01 Berneslai Lifecycle-01',
+    color: 'bg-yellow'
+  },
+  {
+    ShortDescription: 'Tree desc',
+    LocList: '01BN - 01 Berneslai Lifecycle-01',
+    color: 'bg-lime'
+  },
+  ];
+  constructor(
+  ) { }
+
+  ngOnInit() {
+    this.dateFormat = this.headerDateDisplay(this.value.getDay() - 1, this.value.getMonth(), this.value.getFullYear());
+  }
+
+
+  EventNavigate(events) {
+    switch (events) {
+      case 'day': this.days = true; this.months = false; this.weeks = false; break;
+      case 'month': this.months = true; this.days = false; this.weeks = false; break;
+      case 'week': this.weeks = true; this.months = false; this.days = false; break;
+    }
+  }
+
+
+
+  dateChange(data) {
+    this.value = data;
+    this.dateFormat = this.headerDateDisplay(this.value.getDay() - 1, this.value.getMonth(), this.value.getFullYear());
+  }
+  navigateToPrevDate() {
+    this.value.setDate(this.value.getDate() - 1);
+  }
+  navigateToNextDate() {
+    this.value.setDate(this.value.getDate() + 1);
+
+  }
+  private headerDateDisplay(d, m, y) {
+    return `${Daylist[d]},${MonthsList[m]} ${m + 1}, ${y}`;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { DatePickerModule } from '@progress/kendo-angular-dateinputs';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    DatePickerModule
   ],
   providers: [],
+  exports: [DatePickerModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
